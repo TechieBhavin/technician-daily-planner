650610f9cdec6ada3da6a606f2e491efafeb3aea
Patel Bhavin <techie.bhavin@gmail.com>
2026-01-30 20:24:30 +0530

    setup mongodb connection using mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Connect MongoDB
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Technician Daily Planner API is running");
