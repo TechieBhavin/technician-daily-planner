1e9e7284f85a0e6c9f56a4c468b265cf0b414e49
Patel Bhavin <techie.bhavin@gmail.com>
2026-01-30 23:30:14 +0530

    add API to mark task as completed

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -52,7 +52,44 @@ const getTodayTasks = async (req, res) => {
   }
 };
 
+// @desc   Mark task as completed
+// @route  PATCH /api/tasks/:id/complete
+// @access Public
+const completeTask = async (req, res) => {
+  try {
+    const { completedAt } = req.body;
+
+    if (!completedAt) {
+      return res.status(400).json({ message: "Completion time is required" });
+    }
+
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    if (task.status === "Completed") {
+      return res.status(400).json({ message: "Task already completed" });
+    }
+
+    task.status = "Completed";
+    task.completedAt = completedAt;
+
+    await task.save();
+
+    res.status(200).json({
+      message: "Task marked as completed",
+      task,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
 module.exports = {
   createTask,
   getTodayTasks,
+  completeTask
 };
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTask, getTodayTasks } = require("../controllers/taskController");
+const { createTask, getTodayTasks, completeTask } = require("../controllers/taskController");
 
 router.post("/", createTask);
 router.get("/today", getTodayTasks);
+router.patch("/:id/complete", completeTask);
 
 module.exports = router;
